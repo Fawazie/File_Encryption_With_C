89897ba230f5d6770813220c8075ec48d5d14818
Fawazie <fawazabdulwahab@gmail.com>
2025-01-19 23:07:48 -0600

    Fixing Indentation

--- source.c
+++ source.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
 
-
-int get_bitmask(char* key)
+int get_bitmask(char *key)
 {
-				int hash = 0;
-				while(*key)
-								hash+=(*key++);
-				return hash;
+	int hash = 0;
+	while (*key)
+		hash += (*key++);
+	return hash;
 }
-
 
 int encrypt(int c, char *key)
 {
-						int bitmask = get_bitmask(key);
-						return c ^ bitmask;
+	int bitmask = get_bitmask(key);
+	return c ^ bitmask;
 }
 
-
-
-int main(int arc, char* argv[])
+int main(int arc, char *argv[])
 
 {
-				int c;
-				while((c = getchar()) != EOF)
-								putchar(encrypt(c,argv[1]));
+	int c;
+	while ((c = getchar()) != EOF)
+		putchar(encrypt(c, argv[1]));
 }
